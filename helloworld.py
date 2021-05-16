9b9e1b2763a486ab933937db7abe15e263fa52e9
soaringjupiter <soaringjupiter@gmail.com>
2021-05-16 18:50:45 +0200

    added friendly command line argument

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print('hello world')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--friendly", help="displays a more friendly message", action="store_true")
+args = parser.parse_args()
+if args.friendly:
+	print('hello world :)')
+else:
+	print('hello world')
